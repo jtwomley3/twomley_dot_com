2a2378c5ababe48ad5955c2f16c99fd9d841572b
Jared Twomley <jaredtwomley@gmail.com>
2017-03-27 22:54:01 -0400

    added list item updates and a trash can element icon

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
+console.log('js Loaded');
+
  $(function(){
 
-   function addListItem(){
 
-     
-     return
+   function addListItem(){
+    let val = $('#list_item').val();
+
+    if (val.length){
+      $('#shoppingList').append('<li>' + val +
+      "<img class='delete-icon' src='./images/Trash-can.png' alt='trashcan'></li>")
+
+      $('#list_item').val(null)
+     }
+
+     return //console.log('button was clicked. Value = ' + val);
    }
+
+  //  function removeListItem(el){
+  //    return (console.log(el);)
+  //  }
+
+
+
 
 
 
    $('#submitButton').click(addListItem)
+   $('#list_item').keypress(function(e){
+        e.which === 13 ? addListItem() : ''
+      });
+
+   $('.delete-icon').click(console.log("clicked"));
 
  });
